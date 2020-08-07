2395979f1d7911f3f3bd434d4b238fc293364741
minyanglee <Min-Yang.Lee@noaa.gov>
2020-08-07 14:22:49 -0400

    tighten up paths.

--- R_code/project_logistics/R_paths_libraries.R
+++ R_code/project_logistics/R_paths_libraries.R
@@ -17,16 +17,15 @@ if(!require(foreign)) {
 
 # Set up  variables to point to folders 
 
-my_projdir<- "/home/mlee/Documents/projects/fishcondition"
-my_projdir<-"C:/Users/Min-Yang.Lee/Documents/fishcondition"
+my_projdir<- "/home/mlee/Documents/projects/EconomicFishCondition"
+my_projdir<-"C:/Users/Min-Yang.Lee/Documents/EconomicFishCondition"
 
-
-my_codedir<-file.path(my_projdir,"R_code")
-                    
-extraction_code<-file.path(my_projdir,"data_extraction_processing")
-analysis_code <-file.path(my_codedir, "analysis")
+stata_codedir<-file.path(my_projdir,"stata_code")
 R_codedir <-file.path(my_projdir, "R_code")
-my_adopath <-file.path(my_codedir, "aceprice_ado")
+
+extraction_code<-file.path(my_projdir,"data_extraction_processing")
+analysis_code <-file.path(R_codedir, "analysis")
+my_adopath <-file.path(R_codedir, "aceprice_ado")
 
 
 # setup data folder
@@ -44,9 +43,45 @@ data_intermediate <-file.path(my_datadir, "intermediate")
 # setup results folders 
   
 my_results <-file.path(my_projdir, "results")
-hedonicR_results <-file.path(my_results, "hedonicR")
 
 # setup images folders 
 
 my_images <-file.path(my_projdir, "images")
 exploratory <-file.path(my_images, "exploratory")
+
+
+my_tables <-file.path(my_projdir, "tables")
+
+
+
+
+
+
+
+# https://github.com/Hemken/Statamarkdown/blob/master/R/find_stata.r
+# Search through places that stata is usually installed.
+# I think it searches smallest to highest, which means if you have StataIC and stataMP-64, it will stop at StataIC
+# and not pick up StataMP-64 
+stataexe <- ""
+
+for (d in c("C:/Program Files","C:/Program Files (x86)")) {
+  if (stataexe=="" & dir.exists(d)) {
+    for (v in seq(20,11,-1)) {
+      dv <- paste(d,paste0("Stata",v), sep="/")
+      if (dir.exists(dv)) {
+        for (f in c("Stata", "StataIC", "StataSE", "StataMP",
+                    "Stata-64", "StataIC-64", "StataSE-64", "StataMP-64")) {
+          dvf <- paste(paste(dv, f, sep="/"), "exe", sep=".")
+          if (file.exists(dvf)) {
+            stataexe <- dvf
+          }
+          if (stataexe != "") break
+        }
+      }
+      if (stataexe != "") break
+    }
+  }
+  if (stataexe != "") break
+}
+rm(list=c("d","f", "dv","v", "dvf"))
+stataexe<-shQuote(stataexe)
